0c316d8e126369db10ea81d9bea83208d87bca32
Roman <41066029+iamr0man@users.noreply.github.com>
2019-07-08 16:23:57 +0200

    rewrite some class in css, add 2 section

--- step-project-ham/js/script.js
+++ step-project-ham/js/script.js
@@ -3,6 +3,7 @@ const $tabsContent = $('.content-item');
 const $decorationTriangular = $('<div></div>').addClass("arrow-down"); //create arrow
 
 $('.content-item:not(:first-child)').hide(); //hide all element instead first
+$('.tabs-title').eq(0).append($decorationTriangular)
 
 let index = 0;
 $tabs.click(function(e){
@@ -13,9 +14,31 @@ $tabs.click(function(e){
     $(e.target).addClass('active');
     $tabsContent.eq(index).show();
 
-    const $halfWidth = $(e.target).innerWidth()/2 - 30; //write half width
-
-    $decorationTriangular.css('left', $halfWidth + "px"); //make position arrow in middle of tabs-title
     $(e.target).append($decorationTriangular)
+})
+
+$('.bar-menu-content-wrap>li:not(:first-child)').hide(); //hide all img of content instead first
+
+$(document).ready(function(){
+    const arr = [];
+
+    for(let i = 1; i <= 12; i++){
+        arr[i] = new Image();
+        arr[i].src = 'img/different_photo/photo' + i + '.jpg'; 
+        arr[i].alt = 'not found'
+
+        $('.all-content').append(arr[i]);      
+    }
+});
+
+let index_i = 0;
+$('#bar-menu').click(function(e){
+    $('.bar-menu-item').eq(index_i).removeClass('active-item');
+    $('.bar-menu-content-wrap>li').eq(index_i).hide();
+
+    index_i = $(e.target).index();
+
+    $(e.target).addClass('active-item');
+    $('.bar-menu-content-wrap>li').eq(index_i).show();
 })
 
