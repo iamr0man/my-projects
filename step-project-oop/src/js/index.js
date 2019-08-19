0c8b5c54bf5e5ab48de187cd952444e78e9027e4
Korniienko Roman <koriny4@gmail.com>
2019-08-19 18:14:31 +0300

    rewrite method add card

--- step-project-oop/src/js/index.js
+++ step-project-oop/src/js/index.js
@@ -28,86 +28,103 @@ function outsideClick(e) {
         modal.style.display = 'none';
     }
 }
-// Click on create 
 
-function createVisit(){
-    closeModal();
-    document.querySelector(".main-block span").style.display = "none";
-    
-    let createdElem = document.createElement("div");
-    createdElem.classList.add("created-elements");
-    mainBlock.appendChild(createdElem);
-    
-    for(let i=0 ;i<listOfInputs.children.length; i++ ){
-        if(listOfInputs.children[i].style.display == "block"){
-        for(let k=0; k<listOfInputs.children[i].children.length;k++){
-            createdElem.insertAdjacentHTML("beforeEnd", `<p>${listOfInputs.children[i].children[k].placeholder}:${listOfInputs.children[i].children[k].value}</p>` );
-                        console.log(listOfInputs.children[i].children[k].value);
-
-        }
-
-        }
-
-    }
-
-    createdElem.style.display = "flex";
-
-}
-
-
-
-
-
-
+const listOfDoctor = document.getElementById('list-of-doctors');
+const listOfInputs = document.getElementById('form-inputs');
 
 class Visit {
-    constructor(target, date, fullName){
+    constructor(target, date, fullName) {
         this.target = target;
         this.date = date;
         this.fullName = fullName;
     }
 
-    static createVisit(doctor, arr){
-        
+    addCard() {
+        let createdElem = document.createElement("ul");
+        createdElem.classList.add("created-elements");
+        mainBlock.appendChild(createdElem);
+        for (let [key, value] of Object.entries(this)) {
+            debugger
+            const li = document.createElement('li');
+            li.innerHTML = `${key}: ${value}`
+            createdElem.appendChild(li);
+        }
+        createdElem.children[2].style.display = 'block';
+        createdElem.style.display = "flex";
     }
 }
 
-class VisitToCardiologist extends Visit{
-    constructor(target,date, fullName,pressure, iWeight, diseases, age){
+class VisitToCardiologist extends Visit {
+    constructor(target, date, fullName, pressure, iWeight, diseases, age) {
         super(target, date, fullName);
         this.pressure = pressure;
         this.iWeight = iWeight;
         this.diseases = diseases;
         this.age = age;
+        this.id = 0;
     }
 }
 
 class VisitToDentist extends Visit {
-    constructor(target,date, fullName,dateLastVist){
+    constructor(target, date, fullName, dateLastVist) {
         super(target, date, fullName);
         this.dateLastVist = dateLastVist;
+        this.id = 1;
     }
 }
 
-class VisitToTherapist extends Visit{
-    constructor(target,date, fullName,age){
+class VisitToTherapist extends Visit {
+    constructor(target, date, fullName, age) {
         super(target, date, fullName);
         this.age = age;
+        this.id = 2;
     }
 }
 
-const listOfDoctor = document.getElementById('list-of-doctors');
-const listOfInputs = document.getElementById('form-inputs');
+let index = 0;
+
+function createVisit() {
+    closeModal();
+    document.querySelector(".main-block span").style.display = "none";
+    
+    const arr = [];
+    let item = '';
+
+    for (let i = 0; i < listOfInputs.children.length; i++) {
+        if (listOfInputs.children[i].style.display == "block") {
+            for (let k = 0; k < listOfInputs.children[i].children.length; k++) {
+                arr.push(listOfInputs.children[i].children[k].value)
+            }
+        }
+    }
+    if (index === 0) {
+        item = new VisitToCardiologist(...arr);
+    } else if (index === 1) {
+        item = new VisitToDentist(...arr);
+    } else {
+        item = new VisitToTherapist(...arr);
+    }
+    Object.defineProperty(item, 'id', {
+        enumerable: false
+    });
+    item.addCard();
+    clearInputs();
+}
+
+function clearInputs(){
+    for(let i = 0; i < listOfInputs.children[index].children.length; i++){
+        listOfInputs.children[index].children[i].value = '';
+    }
+}
 
 listOfDoctor.addEventListener('change', showEntry);
 
-let index = 0;
-function showEntry(e){
+function showEntry(e) {
     listOfInputs.children[index].style.display = 'none';
 
     const nodes = Array.from(listOfDoctor.children);
     const doc = nodes.filter(i => i.value === e.target.value)
-    
+
     index = nodes.indexOf(doc[0]) - 1;
     listOfInputs.children[index].style.display = 'block';
 }
