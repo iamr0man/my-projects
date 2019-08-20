3627328d803e730dc7c6cbe63b01bc0087466cee
iamr0man <koriny4@gmail.com>
2019-08-20 12:51:22 +0300

    add/show from local storage

--- step-project-oop/src/js/index.js
+++ step-project-oop/src/js/index.js
@@ -6,6 +6,40 @@ const createBtn = document.querySelector('.modal-footer button');
 const mainBlock = document.querySelector('.main-block');
 let btnShowMore;
 
+document.addEventListener('DOMContentLoaded', getCards);
+
+function getCards(){
+    let cards;
+    if(localStorage.getItem('cards') === null){
+        cards = [];
+    } else {
+        cards = JSON.parse(localStorage.getItem('cards'))
+    }
+
+    cards.forEach(function(item) {
+        const createdElem = document.createElement("ul");
+
+        createdElem.classList.add("created-elements");
+        mainBlock.appendChild(createdElem);
+
+        for(let i = 0; i < cards.length; i++){
+            for (let [key, value] of Object.entries(cards[i])) {
+            
+                const li = document.createElement('li');
+                li.innerHTML = `${value}`
+                createdElem.appendChild(li);
+            }
+        }
+        
+        createdElem.children[2].style.display = 'block'; // show full name
+
+        const btn = addBtnShowMore();
+        createdElem.appendChild(btn);
+
+        createdElem.style.display = "flex";
+    })
+}
+
 // Events
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
@@ -55,6 +89,8 @@ class Visit {
         createdElem.appendChild(btn);
 
         createdElem.style.display = "flex";
+        debugger;;
+        storeCardInLocalStorage(createdElem.children);
     }
 }
 
@@ -83,6 +119,23 @@ class VisitToTherapist extends Visit {
         this.age = age;
         this.id = 2;
     }
+}
+
+function storeCardInLocalStorage(card){
+    let cards;
+    if(localStorage.getItem('cards') === null){
+        cards = [];
+    } else {
+        cards = JSON.parse(localStorage.getItem('cards'))
+    }
+
+    const temp = [];
+    for(let i = 0; i < card.length; i++){
+        temp.push(card[i].textContent);
+    }
+
+    cards.push(temp);
+    localStorage.setItem('cards', JSON.stringify(cards))
 }
 
 let index = 0;
@@ -143,7 +196,9 @@ function addBtnShowMore() {
 
 function showMore(e) {
     const currentInputs = e.path[1].children;
-    for(let i = 0; i < currentInputs.length; i++){
+    for (let i = 0; i < currentInputs.length; i++) {
         currentInputs[i].style.display = 'block';
     }
+    // console.log(e.target);
+    e.path[1].removeChild(e.target);
 }
